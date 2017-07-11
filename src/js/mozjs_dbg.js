2520a50733510108b251f8d88d756798cf9c6ed3
PhilippRo <rosendahl@otris.de>
2017-07-11 17:12:22 +0200

    add step out command

--- src/js/mozjs_dbg.js
+++ src/js/mozjs_dbg.js
@@ -1451,6 +1451,21 @@
             },
 
             /**
+             * finishes the continuation of the current function body
+             */
+            'step_out': {
+                needPause: true,
+                fn: function(ctx) {
+                    var frame  = ctx.debuggerMediator.
+                                        getPC().getFrame();
+                    frame.older.onStep = function() {
+                        ctx.debuggerMediator.pause();
+                    };
+                    return HC_RES_CONTINUE;
+                 }
+            },
+
+            /**
              * Steps to the next line proceeding through subroutines.
              */
             'next': {
